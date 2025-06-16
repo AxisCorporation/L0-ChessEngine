68c042ed865a8794c089a72ff1e09f223805fe5e
Isaacashar108 <isaacashar108@gmail.com>
2025-06-15 20:32:39 -0500

    Added body to ResetBoard. Uses ChessPiece class

--- Models/ChessBoard.cs
+++ Models/ChessBoard.cs
@@ -15,7 +15,46 @@ public class ChessBoard : IChessBoard
     }
 
     public void MakeMove(IMove move);
-    public void ResetBoard();
+    public void ResetBoard()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                Grid[i, j] = null;
+            }
+        }
+
+        // White pieces first
+        Grid[0, 0] = new ChessPiece(PieceType.White | PieceType.Rook);
+        Grid[0, 1] = new ChessPiece(PieceType.White | PieceType.Knight);
+        Grid[0, 2] = new ChessPiece(PieceType.White | PieceType.Bishop);
+        Grid[0, 3] = new ChessPiece(PieceType.White | PieceType.Queen);
+        Grid[0, 4] = new ChessPiece(PieceType.White | PieceType.King);
+        Grid[0, 5] = new ChessPiece(PieceType.White | PieceType.Bishop);
+        Grid[0, 6] = new ChessPiece(PieceType.White | PieceType.Knight);
+        Grid[0, 7] = new ChessPiece(PieceType.White | PieceType.Rook);
+
+        for (int i = 0; i < 8; i++)
+        {
+            Grid[1, i] = new ChessPiece(PieceType.White | PieceType.Pawn);
+        }
+
+        // Black pieces
+        Grid[7, 0] = new ChessPiece(PieceType.Black | PieceType.Rook);
+        Grid[7, 1] = new ChessPiece(PieceType.Black | PieceType.Knight);
+        Grid[7, 2] = new ChessPiece(PieceType.Black | PieceType.Bishop);
+        Grid[7, 3] = new ChessPiece(PieceType.Black | PieceType.Queen);
+        Grid[7, 4] = new ChessPiece(PieceType.Black | PieceType.King);
+        Grid[7, 5] = new ChessPiece(PieceType.Black | PieceType.Bishop);
+        Grid[7, 6] = new ChessPiece(PieceType.Black | PieceType.Knight);
+        Grid[7, 7] = new ChessPiece(PieceType.Black | PieceType.Rook);
+
+        for (int i = 0; i < 8; i++)
+        {
+            Grid[6, i] = new ChessPiece(PieceType.White | PieceType.Pawn);
+        }
+    }
     protected void PrintBoardToTerminal();
     protected bool ReadFEN(int[,] board, string fen);
 }

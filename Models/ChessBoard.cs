07f3b4d508495f9adec1fe6f80999c06276c3251
Isaacashar108 <isaacashar108@gmail.com>
2025-06-15 20:14:16 -0500

    Added constructers for chess board initializing.

---
+++ Models/ChessBoard.cs
@@ -0,0 +1,21 @@
+﻿using L_0_Chess_Engine.Contracts;
+
+namespace L_0_Chess_Engine.Models;
+public class ChessBoard : IChessBoard
+{
+    public IChessPiece[,] Grid { get; set; }
+    public bool IsCheck { get; set; }
+    public bool IsCheckMate { get; set; }
+    public ChessBoard() //constructer to initialize
+    {
+        Grid = new IChessPiece[8, 8];
+        IsCheck = false;
+        IsCheckMate = false;
+        ResetBoard(); // Initialize the board
+    }
+
+    public void MakeMove(IMove move);
+    public void ResetBoard();
+    protected void PrintBoardToTerminal();
+    protected bool ReadFEN(int[,] board, string fen);
+}

5a9cec7c536f202dd411346ab5e96cc4048657a1
Mohammad Daud Khawaja <daud.khawaja@outlook.com>
2025-06-21 13:36:10 +0500

    Bound pieces updating back to the new chessboard structure

--- Views/ChessBoardView.axaml.cs
+++ Views/ChessBoardView.axaml.cs
@@ -10,53 +10,54 @@ namespace L_0_Chess_Engine.Views;
 public partial class ChessBoardView : UserControl
 {
 
-    private ChessBoardViewModel _viewModel;
+    private ChessBoardViewModel _chessBoardVM;
     private Grid? _chessBoardGrid;
-    private ChessBoard _chessBoard;
 
     public ChessBoardView()
     {
         InitializeComponent();
 
-        _viewModel = new ChessBoardViewModel();
-        _chessBoard = new ChessBoard();
+        _chessBoardVM = new ChessBoardViewModel();
 
-        DataContext = _viewModel;
+        DataContext = _chessBoardVM;
 
         Loaded += (s, e) =>
         {
             _chessBoardGrid = this.FindControl<Grid>("ChessBoardGrid");
-            if (_chessBoardGrid != null)
-            {
-                CreateChessBoardUI();
-            }
+            CreateChessBoardUI();
         };
+
+        _chessBoardVM.OnBoardChanged += CreateChessBoardUI;
 
     }
 
     private void CreateChessBoardUI()
     {
-       
+        if (_chessBoardGrid is null)
+        {
+            return;
+        }
+        
         for (int row = 0; row < 8; row++)
         {
             for (int col = 0; col < 8; col++)
             {
                 bool isLightSquare = (row + col) % 2 == 0;
                 var background = isLightSquare ? Brushes.Wheat : Brushes.SaddleBrown;
-                
-                
+
+
                 var square = new Grid
                 {
                     Background = background
                 };
-                
+
                 // Add to grid
                 Grid.SetRow(square, row);
                 Grid.SetColumn(square, col);
                 _chessBoardGrid.Children.Add(square);
 
-                var Piece = new ChessPieceViewModel((ChessPiece) _chessBoard.Grid[row, col]);
-                var Image = new Image()
+                var Piece = _chessBoardVM.GridPieces[row * 8 + col];
+                var Image = new Image
                 {
                     Source = Piece.Image
                 };

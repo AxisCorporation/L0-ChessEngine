5c35401bc9e27488bba5f3109712c2561d8da55b
Isaacashar108 <isaacashar108@gmail.com>
2025-06-29 21:17:37 -0500

    Add a main menu with options

--- Views/MainMenuView.axaml.cs
+++ Views/MainMenuView.axaml.cs
@@ -1,16 +1,47 @@
-using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Controls;
 
 namespace L_0_Chess_Engine.Views;
 
 public partial class MainMenuView : UserControl
 {
     public MainWindow? MainWindow { get; set; }
-    
+
     public MainMenuView(MainWindow? mainWindow)
     {
         InitializeComponent();
         MainWindow = mainWindow;
+
+        PlayToggle.Checked += (_, _) =>
+        {
+            PlayToggle.IsVisible = false; // Play itself gone
+            PlayMainOptions.IsVisible = true;
+            PlayMainOptions.Opacity = 1;
+
+            OtherOptions.IsVisible = false; // hide credits/quit
+        };
+
+        LocalButton.Click += (_, _) =>
+        {
+            // hide Local/AI choices
+            PlayMainOptions.IsVisible = false;
+            PlayMainOptions.Opacity = 0;
+
+            // show Local time options
+            LocalTimeOptions.IsVisible = true;
+            LocalTimeOptions.Opacity = 1;
+        };
+
+        AIButton.Click += (_, _) =>
+        {
+            // hide Local/AI choices
+            PlayMainOptions.IsVisible = false;
+            PlayMainOptions.Opacity = 0;
+
+            // show AI time options
+            AITimeOptions.IsVisible = true;
+            AITimeOptions.Opacity = 1;
+        };
     }
 
     public void PlayGame(object? sender, RoutedEventArgs e)

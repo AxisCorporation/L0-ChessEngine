2bef75f1ec71987a080b1701411bad605f5bda21
Isaacashar108 <isaacashar108@gmail.com>
2025-07-01 11:17:24 -0500

    added fonts folder with font(couldn't get it to work but the asset is there). Added test cases for ReadFen

--- Tests/AllTests.cs
+++ Tests/AllTests.cs
@@ -1,6 +1,45 @@
+using L_0_Chess_Engine.Models;
+using System;
+
 namespace L_0_Chess_Engine.Tests;
 
 class AllTests
 {
-    
+
+    static void TestReadFENValid()
+    {
+        var board = new ChessBoard();
+        bool result = board.ReadFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+
+        if (result==false)
+            throw new Exception("TestReadFENValid failed since ReadFEN returned false");
+
+        if (board.Grid[0, 0].Type != (PieceType.Rook | PieceType.Black))
+            throw new Exception("TestReadFENValid failed since top-left piece not correct");
+
+        Console.WriteLine("TestReadFENValid passed.");
+    }
+
+    static void TestReadFENInvalidRow()
+    {
+        var board = new ChessBoard();
+        bool result = board.ReadFEN("8/8/8/8/8/8/8"); // only 7 rows
+
+        if (result==true)
+            throw new Exception("TestReadFENInvalidRow failed since should return false");
+
+        Console.WriteLine("TestReadFENInvalidRow passed.");
+    }
+
+    static void TestReadFENInvalidCharacter()
+    {
+        var board = new ChessBoard();
+        bool result = board.ReadFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBXR"); // X is invalid
+
+        if (result==true)
+            throw new Exception("TestReadFENInvalidCharacter failed since should return false");
+
+        Console.WriteLine("TestReadFENInvalidCharacter passed.");
+    }
+
 }

72677e8e97a0627f4c3d79d2ccec8ba746c63d23
Isaacashar108 <isaacashar108@gmail.com>
2025-07-17 20:05:22 -0500

    implement ai v1

--- AI/Ai.cs
+++ AI/Ai.cs
@@ -1,42 +1,72 @@
 
 // We will have to change the structure here later, bit for now this is fine.
+using L_0_Chess_Engine.Models;
 using System;
 using System.Collections.Generic;
-using L_0_Chess_Engine.Models;
+using System.Linq;
 
 namespace L_0_Chess_Engine.AI
 {
     class Ai
     {
+        private readonly Random _random = new();
+        private Move? _lastMove;
+
         public Move GenerateMove()
         {
-            var moves = new List<Move>{};
+            var board = ChessBoard.Instance;
+            var moves = new List<Move>();
 
+            //Collect all valid black moves
             for (int x = 0; x < 8; x++)
             {
                 for (int y = 0; y < 8; y++)
                 {
-                    if (!ChessBoard.Instance.Grid[x, y].IsWhite)
-                    {
-                        for (int i = 0; i < 8; i++)
-                        {
-                            for (int j = 0; j < 8; j++)
-                            {
-                                Move move = new Move(ChessBoard.Instance.Grid[x, y], ChessBoard.Instance.Grid[i, j]);
+                    var piece = board.Grid[x, y];
+                    if (piece.Type == PieceType.Empty || piece.IsWhite)
+                        continue;
 
-                                if (move.IsValid) moves.Add(move);
-                            }
-                        }
+                    foreach (var move in Move.GetPossibleMoves(piece))
+                    {
+                        if (!move.IsValid || board.WouldCauseCheck(move))
+                            continue;
+
+                        moves.Add(move);
                     }
                 }
             }
 
-            var random = new Random();
-            int index = random.Next(moves.Count); // from 0 to moves.Count - 1
+            //Prioritize pawns
+            var pawnMoves = moves.Where(m => m.InitPiece.EqualsUncolored(PieceType.Pawn)).ToList();
 
-            return moves[index];
+            // Only pawns if possible
+            if (pawnMoves.Count > 0)
+                moves = pawnMoves;
+
+            // Repeat previous move backwards
+            if (_lastMove != null)
+            {
+                var backMove = moves.FirstOrDefault(m =>
+                    m.InitPiece == _lastMove.DestPiece &&
+                    m.DestPiece.Coordinates.Equals(_lastMove.InitPiece.Coordinates));
+
+                if (backMove != null)
+                {
+                    _lastMove = backMove;
+                    return backMove;
+                }
+            }
+
+            //Avoid captures / checkmates
+            var safeMoves = moves.Where(m => m.DestPiece == PieceType.Empty && !m.TargetsKing).ToList();
+            if (safeMoves.Count > 0)
+                moves = safeMoves;
+
+            //Random among remaining
+            var selected = moves[_random.Next(moves.Count)];
+            _lastMove = selected;
+            return selected;
         }
-        
     }
-    
+
 }

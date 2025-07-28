af52d8548295a7165f90dbd64b66d1a2250d4995
Mohammad Daud Khawaja <daud.khawaja@outlook.com>
2025-07-28 16:52:40 +0500

    Update Ai.cs

--- AI/Ai.cs
+++ AI/Ai.cs
@@ -1,45 +1,70 @@
 
-// We will have to change the structure here later, bit for now this is fine.
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Controls;
 using L_0_Chess_Engine.Models;
 
 namespace L_0_Chess_Engine.AI
 {
     class Ai
     {
+        bool maximizePlayer = false;
+        private readonly Dictionary<PieceType, int> ValueMap;
+
+        public Ai(bool maxPlayer)
+        {
+            maximizePlayer = maxPlayer;
+
+            // Initialize ValueMap with a respective value for each PieceType
+        }
+
         public Move GenerateMove()
         {
-            var moves = new List<Move>{};
+            var moves = new List<Move> { };
+
+            // Creating a copy of pointer for clarity
+            ChessPiece[,] Grid = ChessBoard.Instance.Grid;
 
             for (int x = 0; x < 8; x++)
             {
                 for (int y = 0; y < 8; y++)
                 {
-                    if (ChessBoard.Instance.Grid[x, y].IsWhite || ChessBoard.Instance.Grid[x, y] == PieceType.Empty)
+                    if (Grid[x, y].IsWhite || Grid[x, y] == PieceType.Empty)
                     {
                         continue;
                     }
-                    
-                    for (int i = 0; i < 8; i++)
-                    {
-                        for (int j = 0; j < 8; j++)
-                        {
-                            Move move = new(ChessBoard.Instance.Grid[x, y], ChessBoard.Instance.Grid[i, j]);
 
-                            if (move.IsValid) moves.Add(move);
-                        }
-                    }
-                    
+                    moves.AddRange(Move.GetPossibleMoves(Grid[x, y]));
                 }
             }
 
-            var random = new Random();
-            int index = random.Next(moves.Count); // from 0 to moves.Count - 1
-
-            return moves[index];
+            return MiniMaxMove(moves);
         }
-        
+
+        private Move MiniMaxMove(List<Move> moves)
+        {
+            // Recursive logic to determine the highest and lowest move score
+            // Return lowest scoring move if maximizePlayer = true
+            return moves[0];
+        }
+
+        private int EvaluateMove(Move move)
+        {
+            int whiteScore = 0;
+            int blackScore = 0;
+
+            // Calculate each side's score based on piece value
+            blackScore += EvaluateBoardPosition();
+
+            return whiteScore - blackScore; // Lower is better for bot
+        }
+
+        private int EvaluateBoardPosition()
+        {
+            // Write an algorithm to determine how good the board position (not counting piece value) is for the bot
+            return 0; 
+        }
     }
     
 }
